65d4da4d6f7c90fd36734d29443dd48af8c2fe01
Neal Davis <davis68@illinois.edu>
2018-01-19 11:00:25 -0600

    OK.

---
+++ convert.py
@@ -0,0 +1,8 @@
+def convertDollarsToCents( dollars ):
+    return dollars * 100
+
+def convertHoursToMinutes( hours ):
+    return hours * 60
+
+def  convertFeetToInches( feet ):
+    return feet * 12
